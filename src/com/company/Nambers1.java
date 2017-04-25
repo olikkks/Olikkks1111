c4c7702ef09583f407f4838eb92e444fbadb89ff
olikk <h_o@bk.ru>
2017-04-25 16:05:51 +0300

    First commit

---
+++ src/com/company/Nambers1.java
@@ -0,0 +1,13 @@
+package com.company;
+
+/**
+ * Created by olikk on 25.04.2017.
+ */
+public class Nambers1 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+        int sum = a+b;
+        System.out.println("a+b=" +sum);
+    }
+}
